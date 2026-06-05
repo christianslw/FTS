3392fe38c7b8f5dbcf52456a1400f289f6f8c696
christianslw <christiansalwa@gmail.com>
2026-06-05 19:44:55 +0200

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://christianslw.github.io',
   output: 'static', // Standard für Astro
   base: '/fts/', // WICHTIG: Hier muss der exakte Name deines Repos stehen!
   vite: {
     plugins: [tailwindcss()]
-  },
-  build: {
-    format: 'directory' // sorgt dafür, dass ordnerbasierte HTML-Strukturen erstellt werden
   }
 });
